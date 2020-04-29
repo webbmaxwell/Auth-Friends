bc9ae33d36f77f54f49f153ca0aeab1318d02a1f
webbmaxwell <webbwrites@gmail.com>
2020-04-28 22:59:47 -0600

    Links render and show different components. Still not rendering data from server.

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -1,18 +1,33 @@
 import React from 'react';
 import PrivateRoute from './PrivateRoute.js';
 
+import { axiosWithAuth } from '../utils/axiosWithAuth';
 
 class Friends extends React.Component {
   state = {
-    Friends: []
+    friends: []
   };
 
   componentDidMount() {
     this.getData();
   }
 
-  getData = () => {};
+  getData = () => {
+    axiosWithAuth()
+    .get("/data")
+    .then(res => {
+      console.log(res)
+      this.setState({
+        })
+      })
+    .catch(err => console.error(err)
+  )};
 
-}
+  render() {
+    return(
+      <h1>Friends</h1>
+    )
+  }
+};
 
 export default Friends;
